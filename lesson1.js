fbbd0ed0eba9a7735ee71dc222d4dcc9a599f9c6
Rimamnyang <emmanuelrimamnayang@gmail.com>
2026-05-11 01:14:27 +0300

    Adding CSS styling

---
+++ lesson1.js
@@ -0,0 +1,37 @@
+//types of variables
+let a = 5;
+let b = 10;
+let t = "Emmanuel";
+let p;
+console.log(typeof t);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof p);
+
+let emmanuel = {
+    name: "Emmanuel",
+    age: 20,
+    gender: "male",
+    hobbie: "coding"
+}
+console.log(typeof emmanuel);
+let w = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(typeof w);
+
+let r = true
+console.log(typeof r);
+
+
+//operators
+let sum = a + b;
+console.log(sum);
+let sub = a - b;
+console.log(sub);
+let mul = a * b;
+console.log(mul);
+let div = a / b;
+console.log(div);
+let mod = a % b;
+console.log(mod);
+
+
