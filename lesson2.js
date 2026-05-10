780fe6a0916228d2c40a3df642123d6eb2e264bd
Rimamnyang <emmanuelrimamnayang@gmail.com>
2026-05-11 01:24:54 +0300

    functions in javascript

---
+++ lesson2.js
@@ -0,0 +1,15 @@
+//functions
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(22,44));
+ function greet(name){
+    return "hello "+name;
+ }
+ console.log(greet("emmanuel"));
+// arrow functions
+const sub = (a,b)=>{
+    return a-b;
+}
+console.log(sub(55,11));
